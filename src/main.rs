856e16dbc9ed4d6928df58b1fb8510341ba416af
Ion Vrabie <vrion69@gmail.com>
2025-11-16 08:56:55 +0200

    First try of a design

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,75 @@
 #![no_std]
 #![no_main]
+#![warn(unused_variables)]
+#![warn(unused_imports)]
+#![warn(dead_code)]
+
 
 use defmt::*;
 use embassy_executor::Spawner;
 
 use embassy_stm32::{
+    peripherals,
     gpio::{Level, Output, OutputType, Speed}, spi::{Config as SpiConfig ,Spi}, time::khz, timer::{
         qei::{Qei, Config as QeiConfig},
         simple_pwm::{PwmPin, SimplePwm},
+        GeneralInstance4Channel,
     }
 };
+
 use embassy_time::{Ticker, Duration};
 use {defmt_rtt as _, panic_probe as _};
 
+mod motor;
+use motor::*;
+
+const MOTOR_COUNT_PER_REV: u16 = 330; 
+
+
+
+#[embassy_executor::task]
+async fn motor_control_task(
+    mut motor1: Motor<'static, peripherals::TIM1, peripherals::TIM2>,
+    mut motor2: Motor<'static, peripherals::TIM1, peripherals::TIM3>,
+    mut motor3: Motor<'static, peripherals::TIM1, peripherals::TIM4>,
+    mut motor4: Motor<'static, peripherals::TIM1, peripherals::TIM5>
+) {
+
+    let mut ticker = Ticker::every(Duration::from_millis(10));
+    motor1.init();
+    motor2.init();
+    motor3.init();
+    motor4.init();
+    loop {
+        ticker.next().await;
+    }
+
+}
+
+#[embassy_executor::task]
+async fn spi_task(mut spi: Spi<'static, embassy_stm32::mode::Async, embassy_stm32::spi::mode::Slave>) {
+    // SPI task implementation
+    let mut count: u8 = 0;
+    let mut spi_write_buffer = [0u8; 5];
+    loop {
+        let mut spi_read_buffer = [0u8; 1];
+        
+        for i in spi_write_buffer.iter_mut() {
+            *i = count;
+        }
+        count = count.wrapping_add(1);
+
+        spi.read(&mut spi_read_buffer).await.unwrap();
+        info!("SPI Received: {:x}", spi_read_buffer[0]);
+        spi.write(&spi_write_buffer).await.unwrap();
+        info!("Data sent {:?}", spi_write_buffer);
+    }
+}
+
+
+
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
     info!("Hello World!");
 
@@ -48,7 +103,7 @@ async fn main(_spawner: Spawner) {
         khz(50),
         Default::default(),
     );
-
+    // Split PWM channels
     let simple_pwm_channels = pwm.split();
     let mut mot1_pwm = simple_pwm_channels.ch1;
     let mut mot2_pwm = simple_pwm_channels.ch2;
@@ -84,58 +139,50 @@ async fn main(_spawner: Spawner) {
         spi_config,
     );
 
+    let motor1 = Motor::new(
+        mot1_pwm,
+        m1_ins_a,
+        m1_ins_b,
+        qei_motor1,
+        MOTOR_COUNT_PER_REV
+    );
 
-    let mut ticker = Ticker::every(Duration::from_millis(10)); 
+    let motor2 = Motor::new(
+        mot2_pwm,
+        m2_ins_a,
+        m2_ins_b,
+        qei_motor2,
+        MOTOR_COUNT_PER_REV
+    );
+
+    let motor3 = Motor::new(
+        mot3_pwm,
+        m3_ins_a,
+        m3_ins_b,
+        qei_motor3,
+        MOTOR_COUNT_PER_REV
+    );
+
+    let motor4 = Motor::new(
+        mot4_pwm,
+        m4_ins_a,
+        m4_ins_b,
+        qei_motor4,
+        MOTOR_COUNT_PER_REV
+    );
+
+    spawner.spawn(motor_control_task(
+        motor1,
+        motor2,
+        motor3,
+        motor4
+    ).unwrap());
+    spawner.spawn(spi_task(spi).unwrap());
+
+    let mut ticker = Ticker::every(Duration::from_millis(100));
+
     loop {
-        // info!("high");
-        let mut spi_read_buffer = [0u8; 1];
-        let spi_write_buffer = [1u8, 2u8, 3u8, 4u8, 5u8];
-        m1_ins_a.set_high();
-        m1_ins_b.set_high();
-        m2_ins_a.set_high();
-        m2_ins_b.set_high();
-        m3_ins_a.set_high();
-        m3_ins_b.set_high();
-        m4_ins_a.set_high();
-        m4_ins_b.set_high();
-        spi.read(&mut spi_read_buffer).await.unwrap();
-        info!("SPI Received: {:x}", spi_read_buffer[0]);
-        spi.write(&spi_write_buffer).await.unwrap();
-        info!("Data sent {:?}", spi_write_buffer);
-        vcc_gpio.set_high();
-        standby_gpio.set_high();
-
-        // info!("Motor1 count: {}", qei_motor1.count());
-        // info!("Motor2 count: {}", qei_motor2.count());
-        // info!("Motor3 count: {}", qei_motor3.count());
-        // info!("Motor4 count: {}", qei_motor4.count());
-
-        mot1_pwm.set_duty_cycle_percent(40);
-        mot2_pwm.set_duty_cycle_percent(50);
-        mot3_pwm.set_duty_cycle_percent(60);
-        mot4_pwm.set_duty_cycle_percent(70);
-        // Timer::after_millis(5).await;
-
-        ticker.next().await;
-
-        // info!("low");
-
-        mot1_pwm.set_duty_cycle_percent(5);
-        mot2_pwm.set_duty_cycle_percent(10);
-        mot3_pwm.set_duty_cycle_percent(15);
-        mot4_pwm.set_duty_cycle_percent(20);
-
-        m1_ins_a.set_low();
-        m1_ins_b.set_low();
-        m2_ins_a.set_low();
-        m2_ins_b.set_low();
-        m3_ins_a.set_low();
-        m3_ins_b.set_low();
-        m4_ins_a.set_low();
-        m4_ins_b.set_low();
-        vcc_gpio.set_low();
-        standby_gpio.set_low();
-
         ticker.next().await;
     }
 }
+    
